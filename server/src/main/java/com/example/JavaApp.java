cf9995f8968b48558ca34ced233420108e64b71f
NivethithaThiru <59823605+NivethithaThiru@users.noreply.github.com>
2022-02-24 00:10:03 +0530

    Update JavaApp.java

---
+++ server/src/main/java/com/example/JavaApp.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class JavaApp {
+
+    public static String message = "Hello Nivethitha!";
+    
+    public JavaApp() {}
+    public static void main(String[] args) {
+        String a = displayMessage();
+        System.out.println(a);
+    }
+
+    public static String displayMessage()
+    {
+        return message;
+    }
+}
